b64d83eea6d8d0a6b6cdd087144e4dc38ec96c34
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2023-09-21 22:48:11 +0100

    changing index.php to display php information

--- src/index.php
+++ src/index.php
@@ -1,10 +1,11 @@
 <?php
 
-$date = date("Y-m-d H:i:s");
+/*$date = date("Y-m-d H:i:s");
 $content = "some text here - $date" . PHP_EOL;
 $fp = fopen("file.txt","a");
 fwrite($fp,$content);
 fclose($fp);
+*/
 
 phpinfo();
 ?>
